40d714b8a9184bf64db7a1473d44ea8645149b51
MaxKopyl <m.s.kopyl.kharkov@gmail.com>
2020-05-28 11:44:52 +0300

    add carousel

--- js/main.js
+++ js/main.js
@@ -1,18 +1,29 @@
 $(document).ready(function(){
-    $('.js-example-templating').select2();
+    // $('.js-example-templating').select2();
+    $(".owl-carousel").owlCarousel();
 })
 
-function formatState(state) {
-    if(!state.id) {
-        return state.text;
-    }
-    var baseUrl = "../img";
-    var $state = $(
-        '<span><img src="' + baseUrl + '/' + state.element.value.toLowerCase() + '.png" class="img-flag" /> ' + state.text + '</span>'
-        );
-    return $state;
-};
+$(".first-carousel").owlCarousel({
+    loop: true,
+    center: true,
+    dots: true,
+    autoplay: true,
+    margin: 0,
+    items: 1,
+    dots: true,
+})
 
-$(".js-example-templating").select2({
-    templateResult: formatState
-});
+// function formatState(state) {
+//     if(!state.id) {
+//         return state.text;
+//     }
+//     var baseUrl = "../img";
+//     var $state = $(
+//         '<span><img src="' + baseUrl + '/' + state.element.value.toLowerCase() + '.png" class="img-flag" /> ' + state.text + '</span>'
+//         );
+//     return $state;
+// };
+
+// $(".js-example-templating").select2({
+//     templateResult: formatState
+// });
